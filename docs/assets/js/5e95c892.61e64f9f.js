94e67ec817a53b6c59bb5a741259972dda1420df
plans-coding <152713201+plans-coding@users.noreply.github.com>
2024-09-05 23:29:14 +0200

    Delete docs directory

--- docs/assets/js/5e95c892.61e64f9f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbewegung=self.webpackChunkbewegung||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>d});r(6540);var u=r(4164),n=r(1003),a=r(7559),c=r(2831),t=r(781),l=r(4848);function d(e){return(0,l.jsx)(n.e3,{className:(0,u.A)(a.G.wrapper.docsPages),children:(0,l.jsx)(t.A,{children:(0,c.v)(e.route.routes)})})}}}]);
--- docs/assets/js/a7456010.73c20d3c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbewegung=self.webpackChunkbewegung||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
